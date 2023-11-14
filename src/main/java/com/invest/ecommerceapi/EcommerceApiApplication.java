1bf4dfd282211c159be48c320e4c40e4e3dbce4e
Aluno Invest <aluno.invest@invest.local>
2023-11-13 20:18:10 -0400

    teste

--- src/main/java/com/invest/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/invest/ecommerceapi/EcommerceApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 public class EcommerceApiApplication {
 	public static void main(String[] args) {
+		// Teste
 		SpringApplication.run(EcommerceApiApplication.class, args);
 	}
 }
